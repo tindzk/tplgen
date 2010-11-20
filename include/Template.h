a7f34b3cd721a08223f5245081ba94d784c212f8
Tim Nieradzik <tim.nieradzik@gmx.de>
2010-11-20 18:16:00 +0100

    Add Template_Print() for all integer types.

--- include/Template.h
+++ include/Template.h
@@ -29,8 +29,6 @@ static inline void Template_PrintLiteral(String s, String *res) {
 	String_Append(res, s);
 }
 
-// ---
-
 static inline overload void Template_Print(Date date, String *res) {
 	Template_PrintDate(date, res);
 }
@@ -39,6 +37,34 @@ static inline overload void Template_Print(String s, String *res) {
 	Template_PrintHtml(s, res);
 }
 
-static inline overload void Template_Print(int val, String *res) {
-	String_Append(res, Integer_ToString(val));
+static inline overload void Template_Print(s8 val, String *res) {
+	String_Append(res, Int8_ToString(val));
+}
+
+static inline overload void Template_Print(s16 val, String *res) {
+	String_Append(res, Int16_ToString(val));
+}
+
+static inline overload void Template_Print(s32 val, String *res) {
+	String_Append(res, Int32_ToString(val));
+}
+
+static inline overload void Template_Print(s64 val, String *res) {
+	String_Append(res, Int64_ToString(val));
+}
+
+static inline overload void Template_Print(u8 val, String *res) {
+	String_Append(res, UInt8_ToString(val));
+}
+
+static inline overload void Template_Print(u16 val, String *res) {
+	String_Append(res, UInt16_ToString(val));
+}
+
+static inline overload void Template_Print(u32 val, String *res) {
+	String_Append(res, UInt32_ToString(val));
+}
+
+static inline overload void Template_Print(u64 val, String *res) {
+	String_Append(res, UInt64_ToString(val));
 }
