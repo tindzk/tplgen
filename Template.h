f97c2ea2fd8f7dce45ec71bbec7f6c93d6df820d
Tim Nieradzik <tim.nieradzik@gmx.de>
2010-12-25 16:39:31 +0100

    Delete unused functions.

--- Template.h
+++ Template.h
@@ -15,48 +15,28 @@ DefineCallback(Template, void, String *);
 		Generic_FromObject(&obj), \
 		meth)
 
-static inline void Template_PrintDate(Date date, String *res) {
+static inline overload void Template_Print(Date date, String *res) {
 	String s = Date_Format(date, false);
 	String_Append(res, s);
 	String_Destroy(&s);
 }
 
-static inline void Template_PrintDateTime(DateTime dt, String *res) {
+static inline overload void Template_Print(DateTime dt, String *res) {
 	String s = DateTime_Format(dt);
 	String_Append(res, s);
 	String_Destroy(&s);
 }
 
-static inline void Template_PrintRFC822(Date_RFC822 date, String *res) {
+static inline overload void Template_Print(Date_RFC822 date, String *res) {
 	String s = Date_RFC822_ToString(date);
 	String_Append(res, s);
 	String_Destroy(&s);
 }
 
-static inline void Template_PrintHtml(String s, String *res) {
+static inline overload void Template_Print(String s, String *res) {
 	String enc = HTML_Entities_Encode(s);
 	String_Append(res, enc);
 	String_Destroy(&enc);
-}
-
-static inline void Template_PrintLiteral(String s, String *res) {
-	String_Append(res, s);
-}
-
-static inline overload void Template_Print(Date date, String *res) {
-	Template_PrintDate(date, res);
-}
-
-static inline overload void Template_Print(DateTime dt, String *res) {
-	Template_PrintDateTime(dt, res);
-}
-
-static inline overload void Template_Print(Date_RFC822 date, String *res) {
-	Template_PrintRFC822(date, res);
-}
-
-static inline overload void Template_Print(String s, String *res) {
-	Template_PrintHtml(s, res);
 }
 
 static inline overload void Template_Print(s8 val, String *res) {
